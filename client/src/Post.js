b36e6ce339996cae76c0ce66c1f95ceaabf00a96
MosesChisz <mosesc@cortexware.co.za>
2024-06-01 11:50:14 +0200

    Login Page

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=1200,735" alt="" />
+        </div>          
+        <div className="texts">
+        <h2>Full-house battery backup coming later this year</h2>
+        <p className="info">
+            <span className="author">Moses Chisango</span>
+            <time>2024-05-29 20:25</time>
+          </p>
+        <p className="summary">EcoFlow releases autonomous lawnmower, AC and fridge | TechCrunch</p>
+        </div>          
+      </div>
+    )
+}
