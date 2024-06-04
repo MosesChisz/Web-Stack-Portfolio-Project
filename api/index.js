3fde23719a93ade59708310ce7414da4d36ae482
MosesChisz <mosesc@cortexware.co.za>
2024-06-04 15:24:54 +0200

    PostPage

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors")
 const mongoose = require("mongoose");
-const User = require("./models/User")
+const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser")
+const multer = require("multer");
+const uploadMiddleware = multer({dest: "uploads/"})
+const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "ahhssj49hddaj78hh5v";
@@ -12,6 +17,8 @@ const secret = "ahhssj49hddaj78hh5v";
 app.use(cors({credentials:true,origin:"http://localhost:3000"}));
 
 app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + '/uploads'))
 
 const databaseConnected = mongoose.connect("mongodb+srv://moseschisango2:Yw6dup44J1ialxsv@cluster0.fae4e20.mongodb.net/mernblog?retryWrites=true&w=majority");
 if (databaseConnected){
@@ -39,12 +46,64 @@ app.post("/login", async (req,res) => {
             //logged in
             jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
                 if (err) throw err;
-                res.cookie("token", token).json("ok");
+                res.cookie("token", token).json({
+                    id:userDoc._id,
+                    username
+                });
             })
             //
         }else{
             res.status(400).json("wrong credentials");
         };
+});
+
+app.get("/profile", (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token,secret, {}, (err,info) => {
+        if (err) throw err;
+        res.json(info);
+    })
+})
+
+app.post("/logout", (req,res) => {
+    res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req,res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path+"."+ext;
+    fs.renameSync(path, newPath);
+
+    const {token} = req.cookies;
+    jwt.verify(token,secret, {}, async (err,info) => {
+        if (err) throw err;
+        const {title,summary,content} = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover:newPath,
+            author:info.id,
+        })
+        res.json(postDoc)       
+    })
+});
+
+app.get("/post", async (req,res) => {
+    res.json(
+        await Post.find()
+        .populate("author", ["username"])
+        .sort({createdAt: -1})
+        .limit(20)
+    );
+});
+
+app.get("/post/:id", async (req,res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id).populate("author", ["username"]);
+    res.json(postDoc)
 })
 
 app.listen(4000);
